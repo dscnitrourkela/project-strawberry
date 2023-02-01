bd1cfcd48cbd0a9781c16cf8a859c6e4ca3dfdc1
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-02-01 15:49:54 +0530

    feat:add colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
       colors: {
         electric: '#db00ff',
         ribbon: '#0047ff',
+        'color-primary' : '#486FFD',
+        'color-secondary' : '#496DF2',
+        'color-tertiary' : '#375DE5',
+        'bg-primary' : '#1B1E1F',
+        'bg-secondary' : '#363839'
       },
       fontFamily: {
-        poppins: ['Poppins'],
+        Inter: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       dropShadow: {
         '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
