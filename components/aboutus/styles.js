7b26d54019202af6d6f3662d52cf28c8f9e06b97
Srishty Mangutte <srishtymangutte@gmail.com>
2023-02-03 06:20:34 +0530

    changes to be uploaded

--- components/aboutus/styles.js
+++ components/aboutus/styles.js
@@ -2,16 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-export function Contain() {
-    let cntn ={
-        backgroundColor:'#1B1E1F', color:'#FFFFFF',padding:'3vw',margin:'2vw'
-    }
-  return (
-    <div style={cntn}>styles</div>
-  )
-}
-
-
 const TextB = styled.p`
 ${
   tw`
@@ -33,23 +23,11 @@ export function TextBox(){
 
 }
 
-/*  let pstyle={  
-            color: '#FFFFFF',
-            backgroundColor: 'grey',
-            width: '50vw',
-            margin:'auto', 
-            padding:'0.5vw',
-            border:'1px solid red',
-            borderColor:'red'}
-*/
-
-
 export function Img(){
     return (<img src={'https://res.cloudinary.com/dxsyc4p2j/image/upload/v1675165055/Rectangle_4492_mqcxrv.png'} ></img>);
 }
 
           
-
 export const Headi = styled.p`
 ${
   tw`
@@ -62,3 +40,12 @@ color: '486FFD',
 borderBottom: '1px',
 borderColor: '486FFD'
 `;
+
+export function Contain() {
+  let cntn ={
+      backgroundColor:'#1B1E1F', color:'#FFFFFF',padding:'3vw',margin:'2vw'
+  }
+return (
+  <div style={cntn}>styles</div>
+)
+}
