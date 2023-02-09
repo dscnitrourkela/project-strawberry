c3db191926287677137379bc16c434b1ff103919
A350 <85403534+Lucif3r-in@users.noreply.github.com>
2023-02-09 12:22:40 +0530

    fix: changes in config

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Container } from '../styles/Common.styles'
-import About from '../components/AboutUs/About'
 
 const IndexPage = () => <Container>Hello World</Container>
 
