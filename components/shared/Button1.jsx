e0e61f9aa20f7808d5b216494547fc88779d4efa
Ashutosh Rath <85403534+Lucif3r-in@users.noreply.github.com>
2023-03-12 18:06:09 +0530

    fix: added hover effect on button

--- components/shared/Button1.jsx
+++ components/shared/Button1.jsx
@@ -15,6 +15,10 @@ const ButtonContainer = styled.button`
   width: ${props => (props.small ? '120px' : '240px')};
   margin-right: ${props => (props.small ? '0px' : '36px')};
   border-radius: ${props => (props.small ? '4px' : '12px')};
+   :hover {
+    transform: scale(1.05);
+    transition: all 300ms ease;
+  }
   @media (max-width: 850px) {
     margin-right: 0px;
     height: ${props => (props.small ? '30px' : '40px')};
